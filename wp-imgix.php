3476e3451089fdcd9023f2b5f39f2a1269822c2f
Ryan Soury <rsoury318@gmail.com>
2020-09-22 18:18:51 +1000

    update version number

--- wp-imgix.php
+++ wp-imgix.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: WP ImgIX
- * Version: 0.11.2
+ * Version: 0.12.0
  * Description: Simple Wordpress ImgIX integration
  * Author: Ryan Soury | Web Doodle
  * Author URI: https://www.webdoodle.com.au/
